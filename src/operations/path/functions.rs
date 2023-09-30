31c1620b7a8a74e61edad7c83c9f6ce4cc70d15e
ZenaroDev <pedro_americo@id.uff.br>
2023-09-30 19:45:55 -0300

    Collect DirEntry of ReadDir in a Vec<DirEntry>

--- src/operations/path/functions.rs
+++ src/operations/path/functions.rs
@@ -1,9 +1,14 @@
 use crate::err_handling::functions::read_dir_collect;
+use std::fs::DirEntry;
 use std::{collections::HashSet, fs::read_dir, sync::Arc};
 
 pub fn extract_files(input: &str) -> HashSet<Arc<str>> {
     let images = match read_dir(input) {
-        Ok(entries) => read_dir_collect(entries),
+        Ok(dir) => {
+            let list: Vec<DirEntry> = dir.filter_map(|entry| entry.ok()).collect();
+
+            read_dir_collect(list)
+        }
         Err(e) => panic!("Error: {}", e),
     };
     return images;
