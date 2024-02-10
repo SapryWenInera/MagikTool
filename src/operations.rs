72a287a67c3eaa2608ecb78868806d24f900fb5a
ZenaroDev <pedro_americo@id.uff.br>
2024-02-09 22:55:24 -0300

    Replace unwrap call with filter_map

--- src/operations.rs
+++ src/operations.rs
@@ -1,26 +1,31 @@
-use tokio::fs::read_dir;
 use std::collections::HashMap;
-use std::path::PathBuf;
 use std::io::Error;
+use std::path::PathBuf;
+use tokio::fs::read_dir;
 
 pub mod convertion;
 pub mod generics;
 //pub mod image;
 pub mod path;
 
-pub async fn index_files<S: AsRef<str>>(
-    input: S,
-) -> Result<HashMap<PathBuf, Box<str>>, Error> {
+pub async fn index_files<S: AsRef<str>>(input: S) -> Result<HashMap<PathBuf, Box<str>>, Error> {
     let mut map = HashMap::new();
     let mut entries = read_dir(input.as_ref()).await?;
 
-    for entry in entries.next_entry().await {
-        let entry = entry.unwrap();
-        let key = entry.path();
-        let value: Box<str> = Box::from(key.clone().to_str().unwrap());
-        let _ = map.insert(key, value);
-    }
+    for entry in entries
+        .next_entry()
+        .await
+        .iter()
+        .filter_map(|entry| match entry {
+            Some(value) => Some(value.path()),
+            None => None,
+        })
+        .map(|path| (path.clone(), String::from(path.to_str().unwrap())))
+    {
+        let (key, value) = entry;
 
+        map.insert(key, Box::from(value));
+    }
     drop(entries);
     return Ok(map);
 }
