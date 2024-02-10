f10af4ad67dc19062b5bd837977b98aa37e67e4b
ZenaroDev <pedro_americo@id.uff.br>
2024-02-10 20:02:26 -0300

    Implement new methods in main function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-mod err_handling;
 mod operations;
 mod parser;
 
-use crate::{
-    err_handling::functions::makedir,
-    operations::{convertion::functions::convert_image, generics::functions::chooser},
-    parser::Parser,
-};
+use std::{collections::HashMap, fs::create_dir_all};
+
+use operations::index_images;
+use tokio::runtime::Runtime;
+
+use crate::parser::Parser;
 
 fn main() {
+    let runtime = Runtime::new().unwrap();
     let mut args = Parser::new();
 
     args.args_parse();
 
-    let _ = makedir(&args.output);
+    if !args.output.exists() && args.input.is_dir() {
+        create_dir_all(args.output).unwrap();
+    };
 
-    let images = chooser(&args.input);
+    let input_map = if args.input.is_dir() {
+        runtime.block_on(index_images(&args.input)).unwrap()
 
-    let _result = convert_image(
-        &args.input,
-        &args.output,
-        images,
-        args.options,
-        &args.format,
-    );
+    } else {
+        let value: Box<str> = Box::from(args.input.to_str().unwrap());
+        let mut image = HashMap::new();
+
+        image.insert(args.input, value);
+
+        image
+    };
 }
