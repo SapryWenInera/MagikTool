ae4f361e39275a864d4e145b09162822abdb64de
ZenaroDev <pedro_americo@id.uff.br>
2024-02-16 17:38:31 -0300

    Enforce -f argument on copy for consistency

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn main() {
 async fn convert_images(input: BTreeMap<Box<Path>, Box<Path>>, args: Vec<&str>) {
     input.par_iter().for_each(|(input_path, output_path)| {
         if input_path.extension() == output_path.extension() {
-            Command::new("cp").arg(input_path.as_os_str()).arg(output_path.as_os_str()).spawn().unwrap().wait().unwrap();
+            Command::new("cp").arg("-f").arg(input_path.as_os_str()).arg(output_path.as_os_str()).spawn().unwrap().wait().unwrap();
         } else {
             Command::new("convert").arg(input_path.as_os_str()).args(args.clone()).arg(output_path.as_os_str()).spawn().unwrap().wait().unwrap();
         }
